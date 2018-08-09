95dab2ab4653c4b56b51e1e8707a75bd7b0314c1
yangbo1014 <chris_bosh@163.com>
2018-08-09 23:58:46 +0800

    第一次提交log和readConfig

---
+++ python_api/interfaceTest/common/Log.py
@@ -0,0 +1,51 @@
+
+#为方便写log的时候方便，单独启用一个线程
+#主要是对输出日志的一些操作，包括输出格式的规定，输出等级的定义以及其他一些输出的定义
+#python标准库是在安装python时自动安装的，不用再下载，而第三方库是之后下载的
+
+import logging
+from datetime import datetime
+import threading
+
+class Log:
+    def __init__(self):
+        global logPath, resultPath, proDir
+        proDir = readConfig.proDir
+        resultPath = os.path.join(proDir, "result")
+
+        if not os.path.exists(resultPath):      # 判断是否存在结果文件夹，如果不存在，则创建一个结果文件
+            os.mkdir(resultPath)
+
+        logPath = os.path.join(resultPath, str(datetime.now().strftime("%Y%m%d%H%M%S")))      # 通过本地时间定义测试结果文件名称
+
+        if not os.path.exists(logPath):         # 判断是否存在测试结果文件，如果不存在，则创建一个文件
+            os.mkdir(logPath)
+
+        self.logger = logging.getLogger()       # 定义logger,如果参数为空，则默认为返回Root对象
+
+        self.logger.setlevel(logging.INFO)      # setlevel方法设置日志级别为“INFO”，信息
+
+        handler = logging.FileHandler(os.path.join(logPath, "output.log"))      # 定义handler
+
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')   #定义formater
+
+        handler.setFormatter(formatter)         # 定义formater
+
+        self.logger.addHandler(handler)         # 添加handler
+
+class MyLog:                                    # 将日志添加到线程之中
+    log = None
+    mutex = threading.lock()
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_log():
+
+        if MyLog.log is None:
+            MyLog.mutex.acquire()
+            MyLog.log = Log()
+            MyLog.mutex.release()
+
+        return MyLog.log
---
+++ python_api/interfaceTest/readConfig.py
@@ -0,0 +1,35 @@
+#配置文件
+
+import os                                                   #对文件或目录进行操作
+import codecs
+import configparser                                         #使用该模块的类ConfigParser使配置文件生效
+
+projectDir = os.path.split(os.path.realpath(__file__))[0]   #获取当前脚本所在路径并分割存入字典，取路径
+configPath = os.path.join(projectDir, "config.ini")         #将路径与文件名拼接
+
+class ReadConfig:
+    def __init__(self):
+        fd = open(configPath)
+        data = fd.read()
+        # remove BOM
+        if data[:3] == codecs.BOM_UTF8:
+            data = data[3:]
+            file = codecs.open(configPath, "w")
+            file.write(data)
+            file.close()
+        fd.close()
+
+        self.cf = configparser.ConfigParser()
+        self.cf.read(configPath)
+
+    def get_email(self, name):                          #获取email中的具体key的value值
+        value = self.cf.get("Email", name)
+        return value
+
+    def get_http(self, name):                           #获取http名称，get(section、option)
+        value = self.cf.get("HTTP", name)
+        return value
+
+    def get_db(self, name):                      #获取数据库名称, https://my.oschina.net/u/3041656/blog/793467
+        value = self.cf.get("database", name)
+        return value
